18c60f9ebb865934421b948ae1a36ae9f3894f0c
ToffeeSta <Wjdgnsrh@gmail.com>
2024-12-26 16:47:56 +0900

    f

---
+++ Tab/Tab/ImageViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ViewController.swift
+//  ImageView
+//
+//  Created by 전정훈 on 9/4/24.
+//
+
+import UIKit
+
+class ImageViewController: UIViewController {
+    var isZoom = false
+    var imgOn: UIImage?
+    var imgOff: UIImage?
+    //변수 선언
+    @IBOutlet var imgView: UIImageView!
+    @IBOutlet var btnResize: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        imgOn = UIImage(named: "lamp_on.png")
+        imgOff = UIImage(named: "lamp_off.png")
+        
+        imgView.image = imgOn
+    }
+    //확대 축소 버튼 함수 선언
+    @IBAction func btnResizeImage(_ sender: UIButton) {
+        let scale:CGFloat = 2.0
+        var newWidth: CGFloat, newHeight: CGFloat
+        
+        if (isZoom) {   //true
+            newWidth = imgView.frame.width/scale
+            newHeight = imgView.frame.height/scale
+            btnResize.setTitle("확대", for: .normal)
+        }
+        else {  //false
+            newWidth = imgView.frame.width*scale
+            newHeight = imgView.frame.height*scale
+            btnResize.setTitle("축소", for: .normal)
+        }
+        imgView.frame.size = CGSize(width: newWidth, height: newHeight)
+        isZoom = !isZoom
+    }
+    //토글 스위치 함수 선언
+    @IBAction func switchImageOnOff(_ sender: UISwitch) {
+        if sender.isOn {
+            imgView.image = imgOn
+        }
+        else {
+            imgView.image = imgOff
+        }
+    }
+}
+
